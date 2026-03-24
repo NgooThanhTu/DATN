ef45e7755954887c84d3668dc35f5ecfbaa1b68e
Cuongdzkkk <cuongdqtb01697@gmail.com>
2026-03-24 09:57:07 +0700

    comment lại code đăng nhập bằng gg

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -5,17 +5,17 @@ import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
-import vue3GoogleLogin from 'vue3-google-login'
+// import vue3GoogleLogin from 'vue3-google-login'
 
 const app = createApp(App)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
+  app.component(key, component)
 }
 
 app.use(ElementPlus)
 app.use(router)
-app.use(vue3GoogleLogin, {
-  clientId: 'THAY_BANG_CLIENT_ID_CUA_BAN_O_DAY'
-})
+// app.use(vue3GoogleLogin, {
+//   clientId: 'THAY_BANG_CLIENT_ID_CUA_BAN_O_DAY'
+// })
 app.mount('#app')
